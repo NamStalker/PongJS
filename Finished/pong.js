7fbf8c6c57c198f709d4e45ed0aa98309ef25c32
Zach <zacharymarvin4@gmail.com>
2019-10-27 21:35:27 -0500

    adding the ball to the canvas.

--- Finished/pong.js
+++ Finished/pong.js
@@ -1,5 +1,16 @@
 const canvas = document.getElementById("pong");
 const context = canvas.getContext("2d");
 
+// Fill the background.
 context.fillStyle = "black";
 context.fillRect(0, 0, 800, 500);
+
+drawBall();
+
+function drawBall(x, y){
+	context.fillStyle = "white";
+	context.beginPath();
+	context.arc(390, 240, 10, 0, Math.PI*2, false);
+	context.closePath();
+	context.fill();
+}
